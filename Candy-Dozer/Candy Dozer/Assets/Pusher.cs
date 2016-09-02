d2df70ffb48ab66c430b2bc873455a5fb0ad717c
maoamaya0420 <amayamao@amayamao-no-MacBook-Pro.local>
2016-09-02 14:39:05 +0900

    プッシャーの作成

---
+++ Candy-Dozer/Candy Dozer/Assets/Pusher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pusher : MonoBehaviour {
+
+	Vector3 startPosition;
+
+	public float amplitude;
+	public float speed;
+
+	void Start()
+	{
+		startPosition = transform.localPosition;
+
+	}
+
+	void Update()
+	{
+		//変異を計算
+		float z = amplitude * Mathf.Sin (Time.time * speed);
+
+		//Zを変異させたポジションに再設定
+		transform.localPosition = startPosition + new Vector3 (0, 0, z);
+
+	}
+}
