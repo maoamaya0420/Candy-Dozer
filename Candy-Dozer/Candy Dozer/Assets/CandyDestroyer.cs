5027e007420ed3a7eaa832e9837293c1a3217aa7
maoamaya0420 <amayamao@amayamao-no-MacBook-Pro.local>
2016-09-02 15:53:51 +0900

    オブジェクト動的生成と削除

---
+++ Candy-Dozer/Candy Dozer/Assets/CandyDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyDestroyer : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Candy") {
+			//オブジェクトを削除
+			Destroy (other.gameObject);
+		}
+	}
+
+}
--- Candy-Dozer/Candy Dozer/Assets/Shooter.cs
+++ Candy-Dozer/Candy Dozer/Assets/Shooter.cs
@@ -3,26 +3,68 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour {
 
-	public GameObject candyPrefab;
+	const int SphereCandyFrequency = 3;
+
+	int sampleCandyCount;
+
+
+	public GameObject[] candyPrefabs;    
+	public GameObject[] candySquarePrefabs;
+	public GameObject candyHolder;
 	public float shotSpeed;
 	public float shotTorque;
+	public float baseWidth;
 
-	
-	// Update is called once per frame
-	void Update () {
+	void Update() {
 
 		if (Input.GetButtonDown ("Fire1"))
 			Shot ();
 	
 	}
 
+	//キャンディのプレファブからランダムに一つ選ぶ
+	GameObject SampleCandy()
+	{
+		GameObject prefab = null;
+
+		//特定の回数に一回丸いキャンディを選択する
+		if (sampleCandyCount % SphereCandyFrequency == 0) {
+			int index = Random.Range (0, candyPrefabs.Length);
+			prefab = candyPrefabs [index];
+		} else {
+			int index = Random.Range (0, candySquarePrefabs.Length);
+			prefab = candySquarePrefabs [index];
+		}
+
+		sampleCandyCount++;
+
+		return prefab;
+	}
+
+	Vector3 GetInstantiatePosition()
+	{
+		//画面のサイズとInputの割合からキャンディ生成のポジションを計算
+		float x = baseWidth *
+		          (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
+		return transform.position + new Vector3 (x, 0, 0);
+	}
+
+
+
 	public void Shot()
 	{
 		//プレファブからCandyオブジェクトを作成
 		GameObject candy = (GameObject)Instantiate (
-			                   candyPrefab,
-			                   transform.position,
-			                   Quaternion.identity);    //Quaternionは回転を表現する構造体で、Quaternion.identifyで回転なしという意味になる
+//			                   candyPrefab,
+//			                   transform.position,
+//			                   Quaternion.identity
+		
+			SampleCandy(),
+			GetInstantiatePosition(),
+			Quaternion.identity );    //Quaternionは回転を表現する構造体で、Quaternion.identifyで回転なしという意味になる
+
+		//生成したCandyオブジェクトの親をCandyHolderに設定する
+		candy.transform.parent = candyHolder.transform;
 
 		//CandyオブジェクトのRigidbodyを取得し力と回転を加える
 		Rigidbody candyRigidBody = candy.GetComponent<Rigidbody> ();
